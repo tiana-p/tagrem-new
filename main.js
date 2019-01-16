be25e05c9630f5d3c090a8d94d2c1517dd23cbf7
tiana-p <theratchetprogrammer@gmail.com>
2019-01-15 21:02:35 -0800

    Created main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+    $(document).ready(function(){
+var thing = $('<h1>test</h1>');
+thing.insertAfter('#bannerContainer');
+});
+
+var stateNames = ["Alabama", "Alaska","Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+            
